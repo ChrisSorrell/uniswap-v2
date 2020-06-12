60577236595ca41f1df391df5aca822b828af5b0
ChrisSorrell <52987387+ChrisSorrell@users.noreply.github.com>
2020-06-11 22:26:29 -0400

    Update UniswapV2Router02.sol

--- contracts/UniswapV2Router02.sol
+++ contracts/UniswapV2Router02.sol
@@ -6,8 +6,7 @@ import 'https://github.com/Uniswap/uniswap-lib/blob/master/contracts/libraries/T
 import './interfaces/IUniswapV2Factory.sol';
 import './interfaces/IUniswapV2Router02.sol';
 import './libraries/UniswapV2Library.sol';
-import './libraries/SafeMath.sol';
-import './interfaces/IERC20.sol';
+
 import './interfaces/IWETH.sol';
 
 contract UniswapV2Router02 is IUniswapV2Router02 {
